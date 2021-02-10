38635ef4a46f9f7399e4b7d5f24a4bd178e7240f
Tony <75085728+user537@users.noreply.github.com>
2021-02-10 12:01:03 +0300

    Create franku.py
    Soon there will be a game based on FF lore

---
+++ franku.py
@@ -0,0 +1,8 @@
+print('How much chromosomes do you have?')
+a = int(input())
+if a < 47:
+     print('You need',47-a,'chromosomes, Frank! Comeback later!')
+elif a == 47:
+    print('Ore wa ochinchin ga daisuki nandayo!')
+elif a > 47:
+    print('You have too much chromosomes, Frank. Throw dat shit up!')
